ad6e7a0d3bdc4641d3396fb6772850ea33e4d51f
Stephen McNearney <stephen@hownow.media>
2021-01-14 15:03:41 -0500

    Initial Commit. DigitalCrafts Week 5 Day 4 - HTML Forms Exercises

---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+'use strict'
+
+$(function() {
+
+    var owner = $('#owner');
+    var cardNumber = $('#cardNumber');
+    var cardNumberField = $('#card-number-field');
+    var CVV = $("#cvv");
+    var mastercard = $("#mastercard");
+    var confirmButton = $('#confirm-purchase');
+    var visa = $("#visa");
+    var amex = $("#amex");
+
+    // Use the payform library to format and validate
+    // the payment fields.
+
+    cardNumber.payform('formatCardNumber');
+    CVV.payform('formatCardCVC');
+
+
+    cardNumber.keyup(function() {
+
+        amex.removeClass('transparent');
+        visa.removeClass('transparent');
+        mastercard.removeClass('transparent');
+
+        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+            cardNumberField.addClass('has-error');
+        } else {
+            cardNumberField.removeClass('has-error');
+            cardNumberField.addClass('has-success');
+        }
+
+        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+            mastercard.addClass('transparent');
+            amex.addClass('transparent');
+        } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
+            mastercard.addClass('transparent');
+            visa.addClass('transparent');
+        } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+            amex.addClass('transparent');
+            visa.addClass('transparent');
+        }
+    });
+
+    confirmButton.click(function(e) {
+
+        e.preventDefault();
+
+        var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+        var isCvvValid = $.payform.validateCardCVC(CVV.val());
+
+        if(owner.val().length < 3){
+            alert("Please enter a valid Cardholder Name");
+        } else if (!isCardValid) {
+            alert("Please enter a valid Card Number");
+        } else if (!isCvvValid) {
+            alert("Please enter a valid CVV");
+        } else {
+            // Everything is correct. Add your form submission code here.
+            alert("Everything is correct");
+        }
+    });
+});
